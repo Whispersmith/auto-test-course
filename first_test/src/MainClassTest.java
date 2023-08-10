d1e6d598711dfb3f19a9f523622e3d69ddd8f9dd
Whispersmith <78102201+Whispersmith@users.noreply.github.com>
2023-08-10 17:59:49 +0300

    Test 2
    Commit for Test 2

--- first_test/src/MainClassTest.java
+++ first_test/src/MainClassTest.java
@@ -4,11 +4,21 @@ import org.junit.Test;
 public class MainClassTest extends MainClass{
    @Test
     public void testGetLocalNumber(){
+       System.out.println("Start testGetLocalNumber");
         if(this.getLocalNumber() != 14){
             System.out.println("The local number is " + this.getLocalNumber());
             Assert.fail("The local number has the wrong value");
         }else {
             System.out.println("The local number has the right value: " + this.getLocalNumber());
+        }
+    }
+    @Test
+    public void testGetClassNumber(){
+        System.out.println("Start testGetClassNumber");
+        if(this.getClass_number() > 45){
+            System.out.println("The local number is bigger than 45. It is " + this.getClass_number());
+        }else {
+            System.out.println("The local number is smaller than 45. It is " + this.getClass_number());
         }
     }
 }
