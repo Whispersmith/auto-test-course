162f5e064c631ccb9b28b52ae89a4bc2cc9b97d1
Whispersmith <78102201+Whispersmith@users.noreply.github.com>
2023-08-10 18:48:48 +0300

    Test 3
    Commit for home work test 3.

--- first_test/src/MainClass.java
+++ first_test/src/MainClass.java
@@ -1,5 +1,10 @@
 public class MainClass {
     private int class_number = 20;
+    private String class_string = "Hello, world";
+
+    public String getClass_string(){
+        return class_string;
+    }
 
     public int getClass_number(){
         return class_number;
--- first_test/src/MainClassTest.java
+++ first_test/src/MainClassTest.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Objects;
+
 public class MainClassTest extends MainClass{
    @Test
     public void testGetLocalNumber(){
@@ -20,5 +22,18 @@ public class MainClassTest extends MainClass{
         }else {
             System.out.println("The local number is smaller than 45. It is " + this.getClass_number());
         }
+    }
+
+    @Test
+    public void testGetClassString(){
+       System.out.println("\nStart testGetClassString");
+
+       String first_sample_string = "Hello";
+       String second_sample_string = "hello";
+       if(this.getClass_string().contains(first_sample_string) || this.getClass_string().contains(second_sample_string)){
+          System.out.println("Test success passed.");
+       }else{
+          Assert.fail("String doesn't have any substrings.");
+      }
     }
 }
